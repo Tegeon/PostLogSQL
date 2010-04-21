1af6c9a7d0347321639d337e885032c4d520996f
Tommaso Visconti <tommaso.visconti@gmail.com>
2010-04-21 14:52:58 +0200

    Default 0 per il codice di errore

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE `postfix_logs` (
 	`message_id` varchar(128) NOT NULL default '',
 	`delivery_success` enum('yes','no') NOT NULL default 'no',
 	`status` text NOT NULL,
-	`status_code` int unsigned NULL,
+	`status_code` int unsigned NOT NULL defualt 0,
 	`hostname` varchar(128) NOT NULL default '',
 	`start_time` datetime DEFAULT NULL,
 	`last_update` datetime DEFAULT NULL,
